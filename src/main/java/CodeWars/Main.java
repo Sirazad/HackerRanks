8f45dae9309bae1e96b687f475e45283d3947b8c
Sirazad <lajosimola2@gmail.com>
2021-03-13 02:00:29 +0100

    count frequency of char in a String
    given a text, return the count of occurances of the most frequent char in the array.

--- src/main/java/CodeWars/Main.java
+++ src/main/java/CodeWars/Main.java
@@ -1,13 +1,20 @@
 package CodeWars;
 
 import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
 
 public class Main {
     static int[] x = {1, 2, 2, 1, 3, 2, 4, 6, 5, 2, 7};
+    static String word = "aabBcde1321";
 
 
     public static void main(String[] args) {
-        System.out.println(mostFrequentItemCount(x));
+    //    System.out.println(mostFrequentItemCount(x));
+        System.out.println(duplicateCount(word));
     }
 
     /**
@@ -30,4 +37,26 @@ public class Main {
         }
         return map.values().stream().max(Integer::compare).get();
     }
+
+    /**
+     * Given a text, count how many of the characters are present at least twice in the text
+     * @param text the String obtained as source
+     * @return int the value of the chars that are there more then once
+     */
+
+    static public int duplicateCount(String text) {
+        return (int) Arrays.stream(text.split("")).collect(Collectors.groupingBy(
+                Function.identity(), Collectors.counting())).values().stream().mapToLong(n->n).max().getAsLong();
+    }
+
+    /**
+     * Given a text, count the occurance of the most frequent character in the text
+     * @param text the String obtained as source
+     * @return int
+     */
+    static public int frequencyCount(String text) {
+        return (int) Arrays.stream(text.split("")).collect(Collectors.groupingBy(
+                Function.identity(), Collectors.counting())).values().stream().mapToLong(n->n).max().getAsLong();
+    }
+
 }
