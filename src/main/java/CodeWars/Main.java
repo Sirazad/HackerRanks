1a0950286f1b690247a00d3ed3c7e433abc43baf
Sirazad <lajosimola2@gmail.com>
2021-03-14 13:31:49 +0100

    remove duplicates and check if words are anagrams

--- src/main/java/CodeWars/Main.java
+++ src/main/java/CodeWars/Main.java
@@ -10,11 +10,16 @@ import static java.util.stream.Collectors.maxBy;
 public class Main {
     static int[] x = {1, 2, 2, 1, 3, 2, 4, 6, 5, 2, 7};
     static String word = "aabBcde1321";
+    static String anagram = "aabBcde1132";
+    static String notAnagram = "aabBcde1325";
 
 
     public static void main(String[] args) {
     //    System.out.println(mostFrequentItemCount(x));
-        System.out.println(duplicateCount(word));
+    //    System.out.println(duplicateCount(word));
+        System.out.println(isAnagram(word, anagram));
+        System.out.println(isAnagram(word, notAnagram));
+
     }
 
     /**
@@ -39,14 +44,43 @@ public class Main {
     }
 
     /**
+     * check if two strings are anagrams (aka, all letters are the same)
+     * @param test
+     * @param original
+     * @return boolean true if they are anagram, and false if not
+     */
+    public static boolean isAnagram(String test, String original) {
+        test = Arrays.stream(test.split("")).sorted(String::compareTo).collect(Collectors.joining(""));
+        original = Arrays.stream(original.split("")).sorted(String::compareTo).collect(Collectors.joining(""));
+        System.out.println(test+" "+ original);
+        return (test.equals(original));
+    }
+
+
+
+    /**
+     * given a string, remove all words that are duplicate
+     */
+    public static String removeDuplicateWords(String s){
+        return new LinkedHashSet(Arrays.asList(s.split(" "))).toString().replaceAll(",", "").replaceAll("\\[\\]","");
+    }
+
+
+    /**
      * Given a text, count how many of the characters are present at least twice in the text
      * @param text the String obtained as source
      * @return int the value of the chars that are there more then once
      */
 
     static public int duplicateCount(String text) {
-        return (int) Arrays.stream(text.split("")).collect(Collectors.groupingBy(
-                Function.identity(), Collectors.counting())).values().stream().mapToLong(n->n).max().getAsLong();
+        Map<String, Long> myMap = Arrays.stream(text.toLowerCase().split("")).collect(Collectors.groupingBy(
+                Function.identity(), Collectors.counting()));
+        Set<String> mySet = myMap.keySet();
+        for(String string : mySet){
+            System.out.println(string);
+
+        }
+        return 0;
     }
 
     /**
